429082128de2c27746c8cb4cbe05c14c9515f0a9
iso_elk <iso_elk@iso>
2023-08-15 17:42:37 +0100

    I sometimes suffer

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -5,7 +5,7 @@
  * main - Prints the last digit of a randomly generated number
  * and is greater than 5 and less than 6 or 0. 
  *
- * Return: always 0.
+ * Return: Always 0.
  */
 
 int main(void)

c5b94888eb41f4b6d269af58ef46923630f88901
iso_elk <iso_elk@iso>
2023-08-18 21:49:52 +0100

    print last digit

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,15 +1,20 @@
 #include "main.h"
-/**
- * add - adds two numbers and return the result
- * @a : int 
- * @b : int 
- * Return : um of a and b
- */
 
-int add(int a, int b) 
+
+/**
+* add - adds two integers and returns the result
+* @a: int to be added to b
+* @b: int to be added to a
+* Return: sum of a and b
+*/
+
+int add(int a, int b)
 {
-	int c; 
-	
-	c = a + b
-	return c
+	int c;
+
+
+	c = a + b;
+
+
+	return (c);
 }

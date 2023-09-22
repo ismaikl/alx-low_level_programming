f461275a791d33536e81d8dfb5fbd1dd8df5289b
ismaikl <face123moka@gmail.com>
2023-09-22 06:14:56 +0100

    still here

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,84 +1,44 @@
 #include <stdio.h>
-#include "main.h"
+#include <stdlib.h>
+#include <ctype.h>
+
 /**
- * _atoi - converts a string to an integer
- * @s: string to be converted
- *
- * Return: the int converted from the string
+ * is_number - Tests if a string is a number.
+ * @s: The string to check.
+ * Return: 1 if s is a number. 0 otherwise.
  */
-int _atoi(char *s)
+int is_number(char *s)
 {
-	int i, d, n, len, f, digit;
-
-	i = 0;
-	d = 0;
-	n = 0;
-	len = 0;
-	f = 0;
-	digit = 0;
-
-	while (s[len] != '\0')
-		len++;
-
-	while (i < len && f == 0)
-	{
-		if (s[i] == '-')
-			++d;
-
-		if (s[i] >= '0' && s[i] <= '9')
-		{
-			digit = s[i] - '0';
-			if (d % 2)
-				digit = -digit;
-			n = n * 10 + digit;
-			f = 1;
-			if (s[i + 1] < '0' || s[i + 1] > '9')
-				break;
-			f = 0;
-		}
-		i++;
-	}
-
-	if (f == 0)
+	if (s[0] == '\0')
+		return (1);
+	if (!isdigit(s[0]))
 		return (0);
-
-	return (n);
+	else
+		return (is_number(s + 1));
+	return (1);
 }
 
 /**
- * main - adds two positive number
- * @argc: number of arguments
- * @argv: array of arguents
- *
- * Return: 0 (Success), or 1 (Success)
+ * main - Sums and prints all integers given as argument.
+ * @argc: The size of the arguments array.
+ * @argv: The array of the arguments.
+ * Return: 0 if all arguments are integers. 1 otherwise.
  */
 int main(int argc, char *argv[])
 {
-	int sum, num, i, j, k;
+	int e = 0, i = 1;
+	long s = 0;
 
-	sum = 0;
-
-	for (i = 1; i < argc; i++)
+	while (i < argc && e == 0)
 	{
-		for (j = 0; argv[i][j] != '\0'; j++)
-		{
-			if (argv[i][j] > '9' || argv[i][j] < '0')
-			{
-				puts("Error");
-				return (1);
-			}
-		}
+		if (is_number(argv[i]))
+			s += atol(argv[i++]);
+		else
+			e = 1;
 	}
-
-	for (k = 1; k < argc; k++)
-	{
-		num = _atoi(argv[k]);
-		if (num >= 0)
-		{
-			sum += num;
-		}
-	}
-
-	printf("%d\n", sum);
-	return (0);
-
+	if (e)
+		printf("Error\n");
+	else
+		printf("%ld\n", s);
+	return (e);
+} 
